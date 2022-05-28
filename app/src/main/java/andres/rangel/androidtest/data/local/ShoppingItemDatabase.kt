3f135307d64681f6561329fb52611453d8ad0d35
AndresRangelMeli <andres.rangel@mercadolibre.com.co>
2022-05-27 20:02:32 -0500

    Setting up project and Room

---
+++ app/src/main/java/andres/rangel/androidtest/data/local/ShoppingItemDatabase.kt
@@ -0,0 +1,13 @@
+package andres.rangel.androidtest.data.local
+
+import androidx.room.Database
+
+@Database(
+    entities = [ShoppingItem::class],
+    version = 1
+)
+abstract class ShoppingItemDatabase {
+
+    abstract fun shoppingDao(): ShoppingDao
+
+}
